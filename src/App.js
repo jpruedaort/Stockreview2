761d6831f6ef693e660c3a05d721eb4e973ced46
Juan Pablo Rueda <jpruedaort@hotmail.com>
2021-03-07 18:20:38 -0500

    continent list added (not iso format)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React  from 'react';
 import './App.css';
+import country from 'country-list-js'
 
 
 
@@ -13,6 +14,7 @@ class App extends React.Component{
       list:[],
       isloaded: false,
       symsearch:"Select Symbol",
+      continentname:"Select Continent"
     
     }
     this.handleChange = this.handleChange.bind(this);
@@ -21,7 +23,7 @@ class App extends React.Component{
 
 
 
-  componentWillMount(){
+  componentWillMount(){ // THis function is for deleteing any duplicates in the array list
     function removeDuplicates(array, key) {
       return array.reduce((accumulator, element) => {
           if (!accumulator.find(el => el[key] === element[key])) {
@@ -30,18 +32,18 @@ class App extends React.Component{
           return accumulator;
         }, []);
     }
-
- 
     let url = 'https://api.twelvedata.com/stocks?apikey=c49c02d84d5e47889a00c45df4cb791e&country=USA&type=Common Stock'  //Url with apikey (Line 6)
     fetch(url).then(response => response.json()).then((result)=>result.data).then(((rawresult)=>removeDuplicates(rawresult,'symbol')))
       .then((results)=> this.setState({isloaded: true, list: results}))      
-
-    
   }
 
+  
   handleChange(e){
-    this.setState({symsearch: e.target.value})
-
+    const value= e.target.value
+    this.setState({
+      ...this.state,
+        [e.target.name]:value
+    })
   }
 
 
@@ -50,33 +52,30 @@ class App extends React.Component{
   render(){
     return (
       <div>
-        {/* <ul>
-          {this.state.list.map(item => (
-            <li key={item.symbol}> 
-              {item.symbol}
-            </li>
-          ))}
-        </ul> */}
-
-
         <form>
-          <label htmlFor="stcname"> Stock symbol: </label>
-          <select name="stcname" id="stcname" onChange={this.handleChange} >
-            <option >{this.state.symsearch}</option>
-            {this.state.list.length > 0 
-              ? this.state.list.map(item => (
-              <option key={item.symbol} >{item.symbol}</option>))
-              : <option> No symbol found </option>
-            
-            }
-          </select>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
+          <label htmlFor="continentname">Continent:</label>
+            <select name="continentname" id="continentname" onChange={this.handleChange}>
+              <option>{this.state.continentname}</option>
+                {country.continents().map(item =>
+                  (<option>{item}</option>)
+                )}
 
-          <p>selected Symbol: {this.state.symsearch}</p>
+            </select>
+        </form>
+
+
+        {/* This is where the symbol is selected */}
+        <form> 
+          <label htmlFor="symsearch"> Stock symbol: </label>
+            <select name="symsearch" id="symsearch" onChange={this.handleChange} >
+              <option >{this.state.symsearch}</option>
+              {this.state.list.length > 0 
+                ? this.state.list.map(item => (
+                <option key={item.symbol} >{item.symbol}</option>))
+                : <option> No symbol found </option>
+              
+              }
+            </select>
 
 
         </form>
