a3db1586e46d3021e07e757863989728f53c1855
Juan Pablo Rueda <jpruedaort@hotmail.com>
2021-03-07 00:19:32 -0500

    Forms had been made, lifecycle partially donde

--- src/App.js
+++ src/App.js
@@ -1,19 +1,77 @@
-import React from 'react';
+import React  from 'react';
 import './App.css';
+
+
+
+//api key: c49c02d84d5e47889a00c45df4cb791e
+
 
 class App extends React.Component{
   constructor(props){
     super(props);
     this.state={
-      
+      list:[],
+      isloaded: false,
+      symsearch:"Select Symbol",
+    
     }
+    this.handleChange = this.handleChange.bind(this);
   }
+
+
+
+
+  componentWillMount(){
+    let url = 'https://api.twelvedata.com/stocks?apikey=c49c02d84d5e47889a00c45df4cb791e&country=USA&type=Common Stock'  //Url with apikey (Line 6)
+    fetch(url).then(response => response.json()).then((result) => this.setState({isloaded: true, list: result.data}))
+
+    
+  }
+
+  handleChange(e){
+    this.setState({symsearch: e.target.value})
+
+  }
+
+
+
 
   render(){
     return (
-      <div className="App">
-        
+      <div>
+        {/* <ul>
+          {this.state.list.map(item => (
+            <li key={item.symbol}> 
+              {item.symbol}
+            </li>
+          ))}
+        </ul> */}
+
+
+        <form>
+          <label htmlFor="stcname"> Stock symbol: </label>
+          <select name="stcname" id="stcname" onChange={this.handleChange} value={this.state.symsearch} >
+            {this.state.list.length > 0 
+              ? this.state.list.map(item => (
+              <option key={item.symbol} >{item.symbol}</option>))
+              : <option> No symbol found </option>
+            
+            }
+          </select>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+
+          <p>selected Symbol: {this.state.symsearch}</p>
+
+
+        </form>
+      
+
       </div>
+
   
     );
   }
